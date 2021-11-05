223d06a45d235ffbf36e44ee11b56b50dcb22d7c
monoluntik <narbkv07@gmail.com>
2021-11-05 11:04:20 +0600

    commit 5

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_girls_db',
-        'USER': 'neu',
-        'PASSWORD' : '1',
-        'HOST' : 'localhost',
-        'PORT' : 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
